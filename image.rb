38abc99ef1da35698b5a32a81f0b11cd76f800b3
Tia Stutson <tmstutson@gmail.com>
2019-11-19 00:57:04 +0000

    image blur 2

--- image.rb
+++ image.rb
@@ -6,17 +6,36 @@ class Image
 	def output_image
 		@data.each {|row| puts row.join}
 	end
+
+	def blur
+		ones = []
+		(0...@data.size).each do |row|
+			(0...@data[row].size).each do |col|
+				if @data[row][col] == 1
+					ones << [row, col]
+				end
+			end
+		end
+
+		ones.each do |row, col|
+			@data[row-1][col] = 1 if row-1 >= 0
+			@data[row][col-1] = 1 if col-1 >= 0
+			@data[row+1][col] = 1 if row+1 < @data.size
+			@data[row][col+1] = 1 if col+1 < @data[row].size
+		end
+	end
 end
 	
 
 
 
 image = Image.new([
+  [1, 0, 0, 0],
   [0, 0, 0, 0],
-  [0, 1, 0, 0],
   [0, 0, 0, 1],
   [0, 0, 0, 0]
 ])
+image.blur
 image.output_image 
 
 
